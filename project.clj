3ce638be84ab8351c5f7762f5b23ba3cdc6c0608
Pedro Viegas <pedroasv@gmail.com>
2014-09-22 16:43:07 -0300

    updating dependencies

--- project.clj
+++ project.clj
@@ -3,21 +3,22 @@
   :url "http://www.github.com/purnam/brahmin"
   :license {:name "The MIT License"
             :url "http://opensource.org/licencses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [im.chit/purnam.native "0.4.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2138"]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2342"]
                                   [im.chit/purnam.core "0.4.3"]
                                   [im.chit/purnam.test "0.4.3"]
-                                  [midje "1.6.0"]]
-                   :plugins [[lein-cljsbuild "1.0.0"]]}}
+                                  [midje "1.6.3"]]
+                   :plugins [[lein-ancient "0.5.5"]
+                             [lein-cljsbuild "1.0.3"]]}}
   :documentation {:files {"doc/index"
-                        {:input "test/brahmin/api_guide.clj"
-                         :title "brahmin"
-                         :sub-title "Category theory morphisms for clojurescript"
-                         :author "Chris Zheng"
-                         :email  "z@caudate.me"
-                         :tracking "UA-31320512-2"}}}
+                          {:input "test/brahmin/api_guide.clj"
+                           :title "brahmin"
+                           :sub-title "Category theory morphisms for clojurescript"
+                           :author "Chris Zheng"
+                           :email "z@caudate.me"
+                           :tracking "UA-31320512-2"}}}
   :cljsbuild {:builds [{:source-paths ["src", "test"]
-                       :compiler {:output-to "target/brahmin.js"
-                                  :optimizations :whitespace
-                                  :pretty-print true}}]})
+                        :compiler {:output-to "target/brahmin.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
