a692e6fc42ebc68c5514d92c91e9a9cf87f031c1
Chris Zheng <z@caudate.me>
2014-10-09 13:33:17 +0800

    Updated project

--- project.clj
+++ project.clj
@@ -1,16 +1,18 @@
-(defproject im.chit/brahmin "0.3.2"
+(defproject im.chit/brahmin "0.3.3-SNAPSHOT"
   :description "Category theory morphisms for clojurescript"
   :url "http://www.github.com/purnam/brahmin"
   :license {:name "The MIT License"
             :url "http://opensource.org/licencses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2342"]
-                 [im.chit/purnam.core "0.4.4"]
-                 [im.chit/purnam.native "0.4.4"]]
-  :profiles {:dev {:dependencies [[im.chit/purnam.test "0.4.4"]
+                 [im.chit/purnam.core "0.5.1"]
+                 [im.chit/purnam.native "0.5.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2356"]
+                                  [im.chit/purnam.test "0.5.1"]
                                   [midje "1.6.3"]]
                    :plugins [[lein-ancient "0.5.5"]
-                             [lein-cljsbuild "1.0.3"]]}}
+                             [lein-midje "3.1.3"]
+                             [lein-cljsbuild "1.0.3"]
+                             [lein-midje-doc "0.0.24"]]}}
   :documentation {:files {"doc/index"
                           {:input "test/brahmin/api_guide.clj"
                            :title "brahmin"
